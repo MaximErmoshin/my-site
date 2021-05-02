7bb765d41a661ef1619bb565e3bb229666fe1f8c
MaximErmoshin <drug114@yandex.ru>
2021-05-02 15:13:59 +0300

    Добавили поля ввола и выбора

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import FirstPage from './pages/firstPage/firstPage';
+import InputPage from './pages/inputPage/inputPage';
 
 
 const App = () => {
   return (
     <div className="App">
       <FirstPage></FirstPage>
-      
+      <InputPage></InputPage>
     </div>
   );
 }
---
+++ src/pages/inputPage/inputPage.jsx
@@ -0,0 +1,18 @@
+const InputPage = () => {
+
+    return (<>
+        <input type="text" />
+        <input type="password" />
+        <input type="number" />
+        <input type="checkbox" />
+        <input type="date" />
+        <input type="datetime" />
+        <select>
+            <option >Россия</option>
+            <option >Китай</option>
+            <option >Аргентина</option>
+        </select>
+    </>);
+}
+
+export default InputPage;
